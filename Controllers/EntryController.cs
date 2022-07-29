10bac8f5ce9c081146330ae5abb91221653a7dd3
xinyuan729 <1310636528@QQ.COM>
2022-07-29 10:19:05 +0800

    添加了EntryController并且对EntryController所需要的Services进行了修改

---
+++ Controllers/EntryController.cs
@@ -0,0 +1,280 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TJ_Games.DBContext;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ThirdParty.Json.LitJson;
+using TJ_Games.Services;
+using TJ_Games.Models;
+
+namespace TJ_Games.Controllers
+{
+    public class EntryController : Controller
+    {
+        private readonly ModelContext _context;   //数据库上下文
+        private BuyerService service;             //后端service
+        private PublishersService PublisherService;
+        private AdministratorService AdministratorService;
+
+        public EntryController(ModelContext context)
+        {
+            _context = context;
+            service = new BuyerService(_context);
+            PublisherService = new PublishersService(_context);
+            AdministratorService = new AdministratorService(_context);
+        }
+
+        // 传输页面
+        public IActionResult BuyerLogIn()
+        {
+            if (Request.Cookies["buyerNickName"] != null)
+            {
+                return Redirect("/Home/Index");
+            }
+            else
+            {
+                return View();
+            }
+        }
+        public IActionResult BuyerSignUp()
+        {
+            if (Request.Cookies["buyerNickName"] != null)
+            {
+                return Redirect("/Home/Index");
+            }
+            else
+            {
+                return View();
+            }
+        }
+        public IActionResult SellerSignUp()
+        {
+            if (Request.Cookies["sellerNickName"] != null)
+            {
+                return Redirect("/SellerBackground/Home");
+            }
+            else
+            {
+                return View();
+            }
+        }
+        public IActionResult SellerLogIn()
+        {
+            if (Request.Cookies["sellerNickName"] != null)
+            {
+                return Redirect("/SellerBackground/Home");
+            }
+            else
+            {
+                return View();
+            }
+        }
+        public IActionResult AdministratorLogIn()
+        {
+            if (Request.Cookies["adminNickName"] != null)
+            {
+                return Redirect("/Admin/AdminWork");
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        public IActionResult BuyerLogOut()    //买家退出登录
+        {
+            if (Request.Cookies["buyerNickName"] != null)
+            {
+                //设置cookie
+                HttpContext.Response.Cookies.Delete("buyerNickName");
+                HttpContext.Response.Cookies.Delete("buyerID");
+                return Redirect("/Home/Index");
+            }
+            else
+            {
+                return Redirect("/Home/Index");
+            }
+        }
+
+        //[HttpPost]
+        public IActionResult BuyerLogInForm([FromBody]  string Login_ID,[FromBody]string Login_Password)
+        {
+            int is_vaild = service.BuyerLogin(Login_ID, Login_Password);
+
+            if(is_vaild == 1)//说明此时验证通过
+            {
+                //设置对应的登录Cookie
+                 HttpContext.Response.Cookies.Append("buyerID", Login_ID, new CookieOptions { Expires = DateTime.Now.AddSeconds(3600) });
+                //跳转到指定位置
+                return Redirect("/Home/Index");
+            }
+            else if(is_vaild == -1)//说明此时没有该用户名
+            {
+                JsonData jsondata = new JsonData();
+                jsondata["STATUS"] = 0;
+                jsondata["REASON"] = "找不到对应的用户名";
+                return Json(jsondata.ToJson());
+            }
+            else if(is_vaild==-2)//说明此时密码错误
+            {
+                JsonData jsondata = new JsonData();
+                jsondata["STATUS"] = 0;
+                jsondata["REASON"] = "用户名对应的密码错误";
+                return Json(jsondata.ToJson());
+            }
+            else//此种情况理论上不会出现，如果出现此种情况，考虑数据传输被恶意攻击了
+            {
+                JsonData jsondata = new JsonData();
+                jsondata["STATUS"] = 0;
+                jsondata["REASON"] = "未知错误";
+                return Json(jsondata.ToJson());
+            }
+        }
+
+        //public IActionResult BuyerSignUpForm([FromBody] BuyerSignUpModel buyer)
+        public IActionResult BuyerSignUpForm()
+        {
+            BuyerSignUpModel buyer=new BuyerSignUpModel { BuyerID="2052433",Password="2052433",BuyerName="孙致远",Birthday=DateTime.Now,Mail="1310636528@QQ.COM",UserType="1"};
+            int is_success = service.BuyerSignup(buyer);
+
+            if( is_success == 1)//说明添加成功
+            {
+                JsonData jsondata = new JsonData();
+                jsondata["STATUS"] = 1;
+                jsondata["SIGNUP"] = "SUCCESS";
+                return Json(jsondata.ToJson());
+            }
+            else if(is_success==-1)//说明此时数据库当中已经有该用户
+            {
+                JsonData jsondata = new JsonData();
+                jsondata["STATUS"] = 0;
+                jsondata["SIGNUP"] = "FAILED";
+                jsondata["REASON"] = "该用户ID已经被注册";
+                return Json(jsondata.ToJson());
+            }
+            else if(is_success==-2)//说明此时数据库保存失败
+            {
+                JsonData jsondata = new JsonData();
+                jsondata["STATUS"] = 0;
+                jsondata["SIGNUP"] = "FAILED";
+                jsondata["REASON"] = "数据库保存失败";
+                return Json(jsondata.ToJson());
+            }
+            else//此种情况理论上不会出现，如果出现此种情况，考虑数据传输被恶意攻击了
+            {
+                JsonData jsondata = new JsonData();
+                jsondata["STATUS"] = 0;
+                jsondata["SIGNUP"] = "FAILED";
+                jsondata["REASON"] = "未知错误";
+                return Json(jsondata.ToJson());
+            }
+        }
+
+        public IActionResult PublisherLogInForm([FromBody] string Login_ID, [FromBody] string Login_Password)
+        {
+            int is_vaild = PublisherService.PublisherLogin(Login_ID, Login_Password);
+
+            if (is_vaild == 1)//说明此时验证通过
+            {
+                //设置对应的登录Cookie
+                HttpContext.Response.Cookies.Append("PublisherID", Login_ID, new CookieOptions { Expires = DateTime.Now.AddSeconds(3600) });
+                //跳转到指定位置
+                return Redirect("/Home/Index");
+            }
+            else if (is_vaild == -1)//说明此时没有该用户名
+            {
+                JsonData jsondata = new JsonData();
+                jsondata["STATUS"] = 0;
+                jsondata["REASON"] = "找不到对应的用户名";
+                return Json(jsondata.ToJson());
+            }
+            else if (is_vaild == -2)//说明此时密码错误
+            {
+                JsonData jsondata = new JsonData();
+                jsondata["STATUS"] = 0;
+                jsondata["REASON"] = "用户名对应的密码错误";
+                return Json(jsondata.ToJson());
+            }
+            else//此种情况理论上不会出现，如果出现此种情况，考虑数据传输被恶意攻击了
+            {
+                JsonData jsondata = new JsonData();
+                jsondata["STATUS"] = 0;
+                jsondata["REASON"] = "未知错误";
+                return Json(jsondata.ToJson());
+            }
+        }
+
+        public IActionResult PublisherSignUpForm([FromBody] PublisherSignUpModel publisher)
+        {
+            int is_success = PublisherService.PublisherSignup(publisher);
+
+            if (is_success == 1)//说明添加成功
+            {
+                JsonData jsondata = new JsonData();
+                jsondata["STATUS"] = 1;
+                jsondata["SIGNUP"] = "SUCCESS";
+                return Json(jsondata.ToJson());
+            }
+            else if (is_success == -1)//说明此时数据库当中已经有该用户
+            {
+                JsonData jsondata = new JsonData();
+                jsondata["STATUS"] = 0;
+                jsondata["SIGNUP"] = "FAILED";
+                jsondata["REASON"] = "该用户ID已经被注册";
+                return Json(jsondata.ToJson());
+            }
+            else if (is_success == -2)//说明此时数据库保存失败
+            {
+                JsonData jsondata = new JsonData();
+                jsondata["STATUS"] = 0;
+                jsondata["SIGNUP"] = "FAILED";
+                jsondata["REASON"] = "数据库保存失败";
+                return Json(jsondata.ToJson());
+            }
+            else//此种情况理论上不会出现，如果出现此种情况，考虑数据传输被恶意攻击了
+            {
+                JsonData jsondata = new JsonData();
+                jsondata["STATUS"] = 0;
+                jsondata["SIGNUP"] = "FAILED";
+                jsondata["REASON"] = "未知错误";
+                return Json(jsondata.ToJson());
+            }
+        }
+
+        public IActionResult AdministratorLogInForm([FromBody] string Login_ID, [FromBody] string Login_Password)
+        {
+            int is_vaild = AdministratorService.AdministratorLogin(Login_ID, Login_Password);
+
+            if (is_vaild == 1)//说明此时验证通过
+            {
+                //设置对应的登录Cookie
+                HttpContext.Response.Cookies.Append("AdministratorID", Login_ID, new CookieOptions { Expires = DateTime.Now.AddSeconds(3600) });
+                //跳转到指定位置
+                return Redirect("/Home/Index");
+            }
+            else if (is_vaild == -1)//说明此时没有该用户名
+            {
+                JsonData jsondata = new JsonData();
+                jsondata["STATUS"] = 0;
+                jsondata["REASON"] = "找不到对应的用户名";
+                return Json(jsondata.ToJson());
+            }
+            else if (is_vaild == -2)//说明此时密码错误
+            {
+                JsonData jsondata = new JsonData();
+                jsondata["STATUS"] = 0;
+                jsondata["REASON"] = "用户名对应的密码错误";
+                return Json(jsondata.ToJson());
+            }
+            else//此种情况理论上不会出现，如果出现此种情况，考虑数据传输被恶意攻击了
+            {
+                JsonData jsondata = new JsonData();
+                jsondata["STATUS"] = 0;
+                jsondata["REASON"] = "未知错误";
+                return Json(jsondata.ToJson());
+            }
+        }
+    }
+}
--- Services/PublishersService.cs
+++ Services/PublishersService.cs
@@ -3,7 +3,10 @@ using Microsoft.EntityFrameworkCore;
 using Newtonsoft.Json;
 using TJ_Games.DBContext;
 using TJ_Games.Models;
-namespace TJ_Games.Service
+using System.Text;
+using System.Security.Cryptography;
+
+namespace TJ_Games.Services
 {
     public class PublishersService
     {
@@ -153,6 +156,78 @@ namespace TJ_Games.Service
                 return true;
             }
             return false;//本项目中不考虑存在两份一样的游戏更新
+        }
+
+
+        public int PublisherLogin(string Publisher_ID, string Publisher_Password)
+        {
+            //如果前端传明文密码,则计算有关HASH256的值
+            SHA256 sha256 = SHA256.Create();
+
+
+            byte[] Check_Password = Encoding.UTF8.GetBytes(Publisher_Password);
+            byte[] Check_Hash = sha256.ComputeHash(Check_Password);
+
+            string check_Hash = BitConverter.ToString(Check_Hash);
+
+
+            //查询有关用户
+            Users users = _context.Users.Where(x => x.UserID == Publisher_ID).FirstOrDefault();
+
+            if (users == null)//说明此用户不存在
+            {
+                return -1;//代表用户不存在
+            }
+            else//说明此时用户存在
+            {
+
+                if (check_Hash == users.Password)//说明密码正确
+                {
+                    return 1;
+                }
+                else//说明密码错误
+                {
+                    return -2;
+                }
+            }
+        }
+
+        public int PublisherSignup(PublisherSignUpModel Publisher)
+        {
+            if (_context.Users.Where(x => x.UserID == Publisher.PublisherID).FirstOrDefault() != null)
+            {
+                return -1;//说明此时数据库当中已经有该用户
+            }
+            else
+            {
+                //如果前端传明文密码,则计算有关HASH256的值
+                SHA256 sha256 = SHA256.Create();
+
+
+                byte[] Check_Password = Encoding.UTF8.GetBytes(Publisher.Password);
+                byte[] Check_Hash = sha256.ComputeHash(Check_Password);
+
+                string Password_Hash = BitConverter.ToString(Check_Hash);
+
+
+
+                //往数据库当中进行添加操作
+                Users users = new Users { UserID = Publisher.PublisherID, UserType = Publisher.UserType, Password = Password_Hash };
+                _context.Users.Add(users);
+                if (_context.SaveChanges() < 0)//说明保存成功
+                {
+                    return -2;//说明数据库保存失败
+                }
+
+                Publishers new_Publishers = new Publishers { PublisherID = Publisher.PublisherID, PublisherName = Publisher.PublisherName, Description= Publisher.Description,HomepageURL=Publisher.HomepageURL,StartTime= Publisher.StartTime };
+                _context.Publishers.Add(new_Publishers);
+                if (_context.SaveChanges() < 0)//说明保存成功
+                {
+                    return -2;//说明数据库保存失败
+                }
+
+                return 1;
+            }
         }
     }
 }
