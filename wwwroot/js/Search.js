05fb5ea0a9c3cf3d26a122e8f397ec3d27df9371
ovaltine0712 <96032430+ovaltine0712@users.noreply.github.com>
2022-08-02 10:21:18 +0800

    Add files via upload

---
+++ wwwroot/js/Search.js
@@ -0,0 +1,30 @@
+﻿let searchshow = new Vue({
+    el: '.searchshow',
+    data: {
+        results: []
+    },
+    methods: {
+        chooseAddress(item, index) {
+            console.log(item.ID)
+            selectGames(item.ID)
+            return item.ID
+        }
+    }
+})
+
+function submitSearch() {
+    if ($("#searchtype").val() == 1) {
+        axios.post("/Search/SearchCommodity", { Commodity_Name: $("#Context").val() })
+            .then(function (response) {
+                console.log(response)
+                searchshow.results = response.data
+            })
+    }
+    else {
+        axios.post("/Search/SearchPublishers", { Publisher: $("#Context").val() })
+            .then(function (response) {
+                console.log(response)
+                searchshow.results = response.data
+            })
+    }
+}
