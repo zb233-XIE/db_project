5227f78b6863fe6dbc1539a43fd1d336ade2a2d1
ovaltine0712 <96032430+ovaltine0712@users.noreply.github.com>
2022-08-29 21:53:47 +0800

    Add files via upload

--- wwwroot/js/index.js
+++ wwwroot/js/index.js
@@ -1,21 +1,27 @@
 ﻿
+
 let show = new Vue({
     el: '.show',
     data: {
-        games:[]
+        games: [],
+        classes: ["休闲", "动作", "多人在线", "模拟", "角色扮演", "冒险"]
     },
     methods: {
         getNew() {
+            //for (let i = 0; i < list.length; i++)
+            //    list[i].className = 'list';
             var that = this
-            axios.get("/shop/ShowNewCommodity")
+            axios.get("/Home/ShowNewCommodity")
                 .then(function (response) {
                     console.log(response)
                     that.games = response.data
                 })
         },
         getHot() {
+            //for (let i = 0; i < list.length; i++)
+            //    list[i].className = 'list';
             var that = this
-            axios.get("/shop/ShowHotCommodity")
+            axios.get("/Home/ShowHotCommodity")
                 .then(function (response) {
                     console.log(response)
                     that.games = response.data
@@ -25,6 +31,16 @@ let show = new Vue({
             console.log(item.ID)
             selectGames(item.ID)
             return item.ID
+        },
+        chooseClass(item, index) {
+            var that=this
+            axios.post("/Home/ShowComodityClassification", { "type": item })
+                .then(function (response) {
+                    console.log(response)
+                    that.games = response.data
+                    //var text = document.getElementsByClassName("dropbtn").innerHTML = "分类：" + response.data[0].classification.toString();
+                    //console.log(document.getElementsByClassName("dropbtn").innerHTML);
+                })
         }
     }
 })
@@ -32,7 +48,7 @@ let show = new Vue({
 function selectGames(id) {
     console.log(id)
     $.ajax({
-        url: "/shop/GetCommodityDetail",
+        url: "/Home/GetCommodityDetail",
         type: "post",
         contentType: "application/json",
         async: false,
@@ -46,27 +62,29 @@ function selectGames(id) {
 }
 
 function getCommoditySlide() {
-    axios.get("/shop/ShowShopCommodity")
+    axios.get("/Home/ShowShopCommodity")
         .then(function (response) {
             console.log(response.data[0].img)
             for (var i = 1; i <= 5; i++) {
-                document.getElementById("slide" + i.toString()).setAttribute("src", response.data[i-1].img)
+                
+                document.getElementById("slide" + i.toString()).setAttribute("src", response.data[i-1].PictureURL)
             }
+            console.log(document.getElementById("slide5").src);
         })
 }
 
-function getActivitySlide() {
-    axios.get("/shop/ShowCurrentActivity")
-        .then(function (response) {
-            console.log(response.data[0].img)
-            for (var i = 6; i <= 8; i++) {
-                document.getElementById("slide" + i.toString()).setAttribute("src", response.data[i - 6].img)
-            }
-        })
-}
+//function getActivitySlide() {
+//    axios.get("/shop/ShowCurrentActivity")
+//        .then(function (response) {
+//            console.log(response.data[0].img)
+//            for (var i = 6; i <= 8; i++) {
+//                document.getElementById("slide" + i.toString()).setAttribute("src", response.data[i - 6].img)
+//            }
+//        })
+//}
 
 var mySwiper = new Swiper('.swiper', {
-    loop: true, // 循环模式选项
+    //loop: true, // 循环模式选项
     autoplay:true,
 
     // 如果需要分页器
@@ -79,25 +97,36 @@ var mySwiper = new Swiper('.swiper', {
     }
 })
 
-let classification = new Vue({
-    el: '.classification',
-    data: {
-        classes:["休闲","动作","多人在线","模拟","角色扮演","冒险"]
-    },
-    methods: {
-        chooseClass(item,index) {
-            axios.post("/shop/ShowComodityClassification", { "classification": item })
-                .then(function (response) {
-                    console.log(response)
-                    show.games = response.data
-                })
-        }
+
+function submitSearch() {
+    if ($("#searchtype").val() == 1) {
+        axios.post("/Search/SearchCommodity", { Commodity_Name: $("#Context").val() })
+            .then(function (response) {
+                console.log(response)
+                //searchshow.results = response.data
+            })
     }
-})
+    else {
+        axios.post("/Search/SearchPublishers", { Publisher: $("#Context").val() })
+            .then(function (response) {
+                console.log(response)
+                //searchshow.results = response.data
+            })
+    }
+    window.location = "/Search/Search"
+}
 
 function start() {
     show.getNew()
     getCommoditySlide()
-    getActivitySlide()
+    /*getActivitySlide()*/
+    //for (let i = 0; i < list.length; i++)
+    //    list[i].className = 'list';
 }
 window.onload = start()
+
+
+function inputChange() {
+    var searchnav = document.getElementById("Context");
+    searchnav.style.border = "none";
+}
